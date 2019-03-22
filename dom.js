500a74dc6665569a7a899a0773f8884dfe3a3eb7
harshaathnere <harshaathnere@gmail.com>
2019-03-22 12:08:45 +0530

    added removed function

--- dom.js
+++ dom.js
@@ -1,5 +1,7 @@
 // init weather object
 const weather = new Weather('Boston' , 'MA');
+// init  ui
+const ui =  new UI();
 //weather.changeLocation('Miami','FL');
 //Get weather on Dom load
 document.addEventListener('DomContentLoaded' , getWeather);
@@ -7,7 +9,7 @@ document.addEventListener('DomContentLoaded' , getWeather);
 function getWeather(){
 weather.getWeather()
 .then(results => {
-    console.log(results);
+  ui.paint(results);
 })
 .catch(err => console.log(err));
 }
